34bed6a123be2edd90df53b338ab2a28a881926a
ref11 <mrefiyudiansah@gmail.com>
2023-10-01 23:19:31 +0700

    commit pertama

---
+++ bin/test_instalansi_dart.dart
@@ -0,0 +1,34 @@
+import 'dart:ffi';
+
+import 'package:test_instalansi_dart/test_instalansi_dart.dart' as test_instalansi_dart;
+void main() {
+
+  //no 2
+// String nama = "agus";
+// String alamat ="kp pateken";
+// String rt = "03";
+
+// print( '$nama  $alamat  $rt');
+
+//no 3
+
+//faktorial dari 10 dari 20 dan 30
+
+var angka1 = 10;
+var angka2 = 20;
+var angka3 = 30;
+
+var hasil1,hasil2,hasil3;
+
+hasil1 = angka1*9*8*7*6*5*4*3*2*1;
+hasil2 = angka2*19*18*17*16*15*14*13*12*11*10*9*8*7*5*4*3*2*1;
+hasil3 = angka3*29*28*27*26*25*24*23*22*21*20*19*18*17*16*15*14*13*12*11*10*9*8*7*6*5*4*3*2*1;
+
+print(hasil1);
+print(hasil2);
+print(hasil3);
+
+
+
+
+}
---
+++ test/test_instalansi_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:test_instalansi_dart/test_instalansi_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
